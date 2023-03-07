cfadaccdc806b52054721def8e114a259ecf51d9
mehetren <125463210+mehetren@users.noreply.github.com>
2023-03-07 10:48:12 +0530

    Update readcontent.py

--- robot_tests/readcontent.py
+++ robot_tests/readcontent.py
@@ -2,7 +2,7 @@ import json
 
 
 def read_json_file():
-    f = open('C:\\api_sample_project\\Read_content\\create_case.json')
+    f = open('create_case.json')
     jsonfile = f.read()
     f.close()
     return jsonfile
